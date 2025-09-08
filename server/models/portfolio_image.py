16e006929119daf3ff5386c1946c9fdafc840542
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-09-08 22:06:14 +0300

    fix: Broken migrations and fixing tablename, and file name on service

--- server/models/portfolio_image.py
+++ server/models/portfolio_image.py
@@ -5,8 +5,10 @@ from datetime import datetime
 
 
 class PortfolioImage(db.Model,SerializerMixin):
+    __tablename__ = "portfolio_item"
+
     id = db.Column(db.Integer, primary_key=True)
-    image_url = db.Column(db.String(255), nullable=False)  # Cloudinary URL
+    image_url = db.Column(db.String(255), nullable=False) 
 
     portfolio_id = db.Column(db.Integer, db.ForeignKey("portfolio_item.id"), nullable=False)
     portfolio = db.relationship("PortfolioItem", back_populates="images")
--- server/models/services.py
+++ server/models/services.py
@@ -2,9 +2,9 @@ from server.extension import db
 from sqlalchemy_serializer import SerializerMixin
 from datetime import datetime
 
-class Services(db.Model,SerializerMixin):
+class Service(db.Model,SerializerMixin):
 
-    __tablename__ = 'services'
+    __tablename__ = 'service'
 
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String,nullable=False)
