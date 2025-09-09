1452f29d5969fa03d3eb6d14cdc6ade11938dc78
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-09-09 12:59:37 +0300

    fix:minor change on service

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -1,5 +1,5 @@
 from .user import User
-from .services import Service
+from .service import Service
 from .booking import Booking
 from .portfolio_image import PortfolioImage
 from .portfolio_item import PortfolioItem
