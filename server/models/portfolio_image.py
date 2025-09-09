2e961d0e944696e3bce4e414bc8babcd1ace0fd0
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-09-09 13:09:55 +0300

    fix:minor change on portfolio model

--- server/models/portfolio_image.py
+++ server/models/portfolio_image.py
@@ -10,7 +10,7 @@ class PortfolioImage(db.Model,SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     image_url = db.Column(db.String(255), nullable=False) 
 
-    portfolio_id = db.Column(db.Integer, db.ForeignKey("portfolio_item.id"), nullable=False)
+    portfolio_id = db.Column(db.Integer, db.ForeignKey("portfolio_items.id"), nullable=False)
     portfolio = db.relationship("PortfolioItem", back_populates="images")
     
 
